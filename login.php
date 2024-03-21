19251df7d6c2c6c8f194eeda5215eaf8ff71bdf4
R21-2000 <rakhafian9010@gmail.com>
2024-03-21 13:12:25 +0700

    login role

--- login.php
+++ login.php
@@ -2,29 +2,38 @@
 
 session_start();
 
-// Check if user is already logged in
-if (isset($_SESSION['username'])) {
-  header("Location: home.php");
-  exit;
-}
-
-// Process login form submission
 if (isset($_POST['login'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // Connect to database
+  // koneksi database
   include_once 'koneksi.php';
 
-  // Query to check if user exists
   $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
   $result = mysqli_query($conn, $sql);
 
-  // If user exists, login and redirect to home page
+  // kalau usernya sudah ada role bisa langsung ke ghome masing masing
   if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
     $_SESSION['username'] = $username;
-    header("Location: home.php");
-    exit;
+    $_SESSION['role'] = $row['role'];
+
+    switch ($row['role']) {
+      case 'admin':
+        header("Location: adminhome.php");
+        exit;
+      case 'waiter':
+        header("Location: waiterhome.php");
+        exit;
+      case 'kasir':
+        header("Location: kasirhome.php");
+        exit;
+      case 'owner':
+        header("Location: ownerhome.php");
+        exit;
+      default:
+        echo "<p style='color: red;'>Login gagal! Role tidak ditemukan.</p>";
+    }
   } else {
     echo "<p style='color: red;'>Login gagal! Username atau password salah.</p>";
   }
